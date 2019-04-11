024c20acc53a608f27a7e23b4019b80be99e5338
Evan Trout <fishyevan@gmail.com>
2019-04-11 13:04:24 -0500

    Add tracking logic

--- cam.py
+++ cam.py
@@ -15,6 +15,15 @@ led_indicator.off()                 # Set initial state to off
 face_cascade = image.HaarCascade("frontalface", stages=25)
 print(face_cascade)
 
+#calculating bounds for movement
+bound = 0.3
+leftBound = (int)(320 * bound)
+rightBound = (int)(320 * (1 - bound))
+upperBound = (int)(240 * bound)
+lowerBound = (int)(240 * (1 - bound))
+boundWidth = rightBound - leftBound
+boundHeight = lowerBound - upperBound
+
 # FPS clock
 clock = time.clock()
 
@@ -28,11 +37,22 @@ while (True):
     # Note: Lower scale factor scales-down the image more and detects smaller objects.
     # Higher threshold results in a higher detection rate, with more false positives.
     objects = img.find_features(face_cascade, threshold=0.75, scale_factor=1.25)
-
+    img.draw_rectangle((leftBound, upperBound, boundWidth, boundHeight))
     # Draw objects
     for r in objects:
         img.draw_rectangle(r)
+        print(r)
+        centerX = r[0] + (r[2] * 0.5)
+        centerY = r[1] + (r[3] * 0.5)
+        if (centerX < leftBound):
+            print("left")
+        elif (centerX > rightBound):
+            print("right")
+        if (centerY < upperBound):
+            print("up")
+        elif (centerY > lowerBound):
+            print("down")
 
     # Print FPS.
     # Note: Actual FPS is higher, streaming the FB makes it slower.
-    print(clock.fps())
+    #print(clock.fps())
